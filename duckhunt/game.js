d19568d63c5a33e45b64550e22a60b450a1e0210
jkaltm01 <joshbk001@yahoo.com>
2015-10-11 20:26:22 -0400

    working game.js code

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,25 @@
-// Replace this with your code...
+//game.js--simple javascript code for displaying background image with 2 ducks
+
+function init() {
+	var canvas = document.getElementById('game_canvas');
+	var ctx = canvas.getContext("2d");
+
+	var bg = new Image();
+	bg.src = 'duckhunt-background.gif';
+	var duck1 = new Image();
+	duck1.src = 'duckhunt_various_sheet.png';
+	var duck2 = new Image();
+	duck2.src = 'duckhunt_various_sheet.png';
+
+	bg.addEventListener("load", function() {
+		ctx.drawImage(bg,0,0);
+
+		duck1.addEventListener("load", function() {
+			ctx.drawImage(duck1,40,115,40,30,80,70,40,30);
+		});	
+
+		duck2.addEventListener("load", function() {
+			ctx.drawImage(duck2,120,115,40,30,120,50,40,30);
+		});
+	});
+}
