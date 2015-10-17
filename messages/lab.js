0afccc936656aed067a744ca2e4f7c4a42af011b
jkaltm01 <joshbk001@yahoo.com>
2015-10-17 16:11:41 -0400

    created data.json file to parse JSON data and print

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,19 @@
-// Your JavaScript goes here...
+//
+
+function parse(){
+
+	var jsondata = new XMLHttpRequest();
+	jsondata.addEventListener("load", reqListener);
+	jsondata.open("GET", "http://tuftsdev.github.io/comp20-jkaltman/messages/data.json", true);
+	jsondata.send();
+}
+
+function reqListener () {
+
+	var parsedObjects = JSON.parse(this.responseText);
+	elem = document.getElementById("messages");
+
+	for (count = 0; count < parsedObjects.length; count++) {
+		elem.innerHTML += "<p>" +  parsedObjects[count]["content"] + " <span>" + parsedObjects[count]["username"] + "</span></p>";
+	}
+}
